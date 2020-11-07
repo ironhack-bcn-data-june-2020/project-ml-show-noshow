978492d676d7933607fb28079a0e14a2b1a9e106
aGimenezO <65399818+aGimenezO@users.noreply.github.com>
2020-11-07 18:59:59 +0100

    DataCleaning file

--- libs/DataCleaning.py
+++ libs/DataCleaning.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+import pandas as pd
+import numpy as np
 
-
-def clean_df(df):
-    return df
-
-# In[ ]:
-
-
-
-
+def transform_dates_to_date_dtype(df,columns):
+    '''
+    This function converts the columns provided from the received dataframe to Date format.
+    INPUTS: 
+        df: Pandas Dataframe
+        columns: Strings List containing the columns that will be converted to datetime format.
+    OUTPUTS:
+        None
+    '''
+    for col in columns:
+        try:
+            df[col] = pd.to_datetime(df[col])
+        except: #TODO CAPTURE ERROR TYPE
+            print('AN ERROR OCURRED')
