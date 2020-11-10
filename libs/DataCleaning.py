1568dc8020d18df9435c6c23816f13fd03a2bd9d
aaronepv <aaron0802416@gmail.com>
2020-11-10 21:26:13 +0100

    outlier functions

--- libs/DataCleaning.py
+++ libs/DataCleaning.py
@@ -8,7 +8,21 @@ def clean_df(df):
     return df
 
 # In[ ]:
+from sklearn.preprocessing import OrdinalEncoder
+def EncodeAlcoholism(df):
+    
+    '''Alcoholism_Num (None= 0 and Low = 1, Moderate=2  High=3) '''
+    enc= OrdinalEncoder(categories=[["None","Low","Moderate","High"]])
+    df["Alcoholism_Num"]=enc.fit_transform(df["Alcoholism"].values.reshape(-1,1)).reshape(df.shape[0])
+    return df
 
 
+def iqr(dataset, column): # Esta funcion detecta los outliers
+    Q1 = np.percentile(dataset[column], 25)
+    Q3 = np.percentile(dataset[column], 75)
+    IQR = Q3 - Q1
 
-
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    
+    return lower, upper
