5c28d7ffd20ae46658d906ec496610054f114f54
Laura Sánchez <laurasg@hotmail.com>
2020-11-12 18:09:26 +0100

    Function transform_date

--- libs/DataCleaning.py
+++ libs/DataCleaning.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-<<<<<<< HEAD
-# In[1]:
-
-
-def clean_df(df):
-    return df
-
-# In[ ]:
-from sklearn.preprocessing import OrdinalEncoder
-def EncodeAlcoholism(df):
-    
-    '''Alcoholism_Num (None= 0 and Low = 1, Moderate=2  High=3) '''
-    enc= OrdinalEncoder(categories=[["None","Low","Moderate","High"]])
-    df["Alcoholism_Num"]=enc.fit_transform(df["Alcoholism"].values.reshape(-1,1)).reshape(df.shape[0])
-    return df
-=======
-
-def iqr(dataset, column): # Esta funcion detecta los outliers
-    Q1 = np.percentile(dataset[column], 25)
-    Q3 = np.percentile(dataset[column], 75)
-    IQR = Q3 - Q1
-
-    lower = Q1 - 1.5 * IQR
-    upper = Q3 + 1.5 * IQR
-    
-    return lower, upper
-=======
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
@@ -51,4 +24,9 @@ def transform_dates_to_date_dtype(df,columns):
 def fill_missing_values_with_knn(df):
     imputer = KNNImputer()
     return imputer.fit_transform(df)
->>>>>>> f58b10217c1e6c3e3749fde319ccc4f20f0856f9
+
+
+def transform_date(df, col):
+    df[‘AppointmentDay_DOW’]=df[col].dt.day_name()
+    df[‘AppointmentDay_month’] = df[col].dt.month_name()
+    return df
